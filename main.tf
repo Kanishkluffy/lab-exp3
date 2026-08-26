086bfb761ad859070b8b3195ac4b76151cd73e86
Kanishkluffy <ktadhithya2006@gmail.com>
2026-08-26 18:52:22 +0530

    Add required_version to satisfy tflint rule

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ module "compute" {
   web_sg_id         = module.network.web_sg_id
   db_sg_id          = module.network.db_sg_id
 }
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
