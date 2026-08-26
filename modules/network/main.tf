a30f340c604d71bef20d949092dee86c07387ce0
Kanishkluffy <ktadhithya2006@gmail.com>
2026-08-26 18:35:29 +0530

    format previous

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block=var.public_subnet_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags                 = { Name = "spm-lab-vpc" }
